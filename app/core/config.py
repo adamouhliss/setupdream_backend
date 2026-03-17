8a3ba6dea19be73299448970b3e4e40f56767baf
Adam Ouhliss <adamouhliss@gmail.com>
2026-03-17 04:57:45 +0000

    Fix CORS Origins decoding string

--- app/core/config.py
+++ app/core/config.py
@@ -13,19 +13,12 @@ class Settings(BaseSettings):
     DATABASE_URL: str = config("DATABASE_URL", default="postgresql://postgres:LwvemmnYzpTiNrJTCateqWzZfDaYDbue@yamabiko.proxy.rlwy.net:48809/railway")
     
     # CORS
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost:3000",
-        "http://localhost:5173", 
-        "http://localhost:5174",
-        "http://localhost:8080",
-        "https://carre-sport.vercel.app",
-        "https://carre-sport-production.up.railway.app",
-    ]
+    BACKEND_CORS_ORIGINS: Union[str, List[str]] = config("BACKEND_CORS_ORIGINS", default="http://localhost:3000,http://localhost:5173,http://localhost:5174,http://localhost:8080,https://carre-sport.vercel.app,https://carre-sport-production.up.railway.app")
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
+            return [i.strip() for i in v.split(",") if i.strip()]
         elif isinstance(v, (list, str)):
             return v
         raise ValueError(v)
